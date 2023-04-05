165a5674d01d05df2ca6845e6f9cd6f440c8f38f
최지연 <choi02647@naver.com>
2023-04-05 15:39:04 +0900

    background isolate channels 예제 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
-import 'package:flutter/material.dart';
+import 'dart:io' show Directory;
 
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart' as uuid;
+
+import 'simple_database.dart';
+
+///////////////////////////////////////////////////////////////////////////////
+// This is the UI which will present the contents of the [SimpleDatabase]. To
+// see where Background Isolate Channels are used see simple_database.dart.
+///////////////////////////////////////////////////////////////////////////////
 void main() {
   runApp(const MyApp());
 }
@@ -7,24 +20,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Background Isolate Channels',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Background Isolate Channels'),
     );
   }
 }
@@ -32,84 +35,116 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() {
+    return _MyHomePageState();
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  /// The database that is running on a background [Isolate]. This is nullable
+  /// because acquiring a [SimpleDatabase] is an asynchronous operation. This
+  /// value is `null` until the database is initialized.
+  SimpleDatabase? _database;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  /// Local cache of the query results returned by the [SimpleDatabase] for the
+  /// UI to render from. It is nullable since querying the results is
+  /// asynchronous. The value is `null` before any result has been received.
+  List<String>? _entries;
+
+  /// What is searched for in the [SimpleDatabase].
+  String _query = '';
+
+  @override
+  void initState() {
+    // Write the value to [SharedPreferences] which will get read on the
+    // [SimpleDatabase]'s isolate. For this example the value is always true
+    // just for demonstration purposes.
+    final Future<void> sharedPreferencesSet = SharedPreferences.getInstance()
+        .then(
+            (sharedPreferences) => sharedPreferences.setBool('isDebug', true));
+    final Future<Directory> tempDirFuture =
+    path_provider.getTemporaryDirectory();
+
+    // Wait until the [SharedPreferences] value is set and the temporary
+    // directory is received before opening the database. If
+    // [sharedPreferencesSet] does not happen before opening the
+    // [SimpleDatabase] there has to be a way to refresh
+    // [_SimpleDatabaseServer]'s [SharedPreferences] cached values.
+    Future.wait([sharedPreferencesSet, tempDirFuture]).then((values) {
+      final Directory? tempDir = values[1] as Directory?;
+      final String dbPath = path.join(tempDir!.path, 'database.db');
+      SimpleDatabase.open(dbPath).then((database) {
+        setState(() {
+          _database = database;
+        });
+        _refresh();
+      });
     });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _database?.stop();
+    super.dispose();
+  }
+
+  /// Performs a find on [SimpleDatabase] with [query] and updates the listed
+  /// contents.
+  void _refresh({String? query}) {
+    if (query != null) {
+      _query = query;
+    }
+    _database!.find(_query).toList().then((entries) {
+      setState(() {
+        _entries = entries;
+      });
+    });
+  }
+
+  /// Adds a UUID and a timestamp to the [SimpleDatabase].
+  void _addDate() {
+    final DateTime now = DateTime.now();
+    final DateFormat formatter =
+    DateFormat('EEEE MMMM d, HH:mm:ss\n${const uuid.Uuid().v4()}');
+    final String formatted = formatter.format(now);
+    _database!.addEntry(formatted).then((_) => _refresh());
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          TextField(
+            onChanged:
+            _database == null ? null : (query) => _refresh(query: query),
+            decoration: const InputDecoration(
+              labelText: 'Search',
+              suffixIcon: Icon(Icons.search),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(title: Text(_entries![index]));
+              },
+              itemCount: _entries?.length ?? 0,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _database == null ? null : _addDate,
+        tooltip: 'Add',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
